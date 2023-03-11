65ffe3861a3283ddf9e98ba642370240a9514c26
ativ-sodo <sodo@ativ.studio>
2023-03-12 04:49:25 +0900

    Add: Initialize sns app

--- genie_backend/genie_backend/settings/base.py
+++ genie_backend/genie_backend/settings/base.py
@@ -33,7 +33,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "graphene_django",
+
     "accounts",
+    "sns",
 ]
 
 # *********************************************
