5b56e4d28fecb808ebc112f8d7f30fa3b28d0284
ativ-sodo <sodo@ativ.studio>
2023-03-12 04:44:24 +0900

    Add: SocialAccount model
    * define SocialAccount model
    * register SocialAccount model on django admin site

--- genie_backend/accounts/admin.py
+++ genie_backend/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from accounts.models import SocialAccount
+from genie_backend.utils.models import BaseModelAdmin
 
-# Register your models here.
+
+class SocialAccountAdmin(BaseModelAdmin):
+    list_display = (
+        "id",
+        "nickname",
+        "pub_key",
+    )
+
+    list_display_links = ("id",)
+    search_fields = ("nickname",)
+
+
+admin.site.register(SocialAccount, SocialAccountAdmin)
+
+admin.site.site_header = "Genie Admin Site"
+admin.site.site_title = "Genie Admin Site"
+admin.site.index_title = "Genie Admin Site"
