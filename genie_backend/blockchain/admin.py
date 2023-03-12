5cb03b10d2326e7f90d0e32354eff33ed5dd4f3a
ativ-sodo <sodo@ativ.studio>
2023-03-12 13:21:21 +0900

    Add: Network model

--- genie_backend/blockchain/admin.py
+++ genie_backend/blockchain/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from genie_backend.utils.models import BaseModelAdmin
+from blockchain.models import Network
 
-# Register your models here.
+
+class NetworkAdmin(BaseModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+
+    list_display_links = ("name",)
+    search_fields = ("name",)
+
+
+admin.site.register(Network, NetworkAdmin)
--- genie_backend/blockchain/models.py
+++ genie_backend/blockchain/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from genie_backend.utils.models import BaseModel
 
-# Create your models here.
+
+class Network(BaseModel):
+    class Meta:
+        verbose_name = "Network"
+        verbose_name_plural = "Network"
+
+    name = models.CharField(
+        verbose_name="Network name",
+        max_length=50,
+        blank=False,
+        null=False,
+        unique=True,
+        help_text="Network name (ex. Solana ...)",
+    )
+
+    def __str__(self):
+        return f"{self.name}"
