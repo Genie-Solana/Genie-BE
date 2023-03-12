3568d56d21dc525cba9b8c6532e8a9b28bbe315c
ativ-sodo <sodo@ativ.studio>
2023-03-12 14:37:29 +0900

    Add: Inbox model

--- genie_backend/accounts/models.py
+++ genie_backend/accounts/models.py
@@ -34,3 +34,45 @@ class SocialAccount(BaseModel):
 
     def __str__(self):
         return f"{self.nickname} - {self.pub_key}"
+
+
+class Inbox(BaseModel):
+    class Meta:
+        verbose_name = "Inbox"
+        verbose_name_plural = "Inbox"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["pub_key", "network", "sns"],
+                name="unique (pub_key, network, sns)",
+            ),
+        ]
+
+    pub_key = models.CharField(
+        verbose_name="public key",
+        max_length=100,
+        blank=False,
+        null=False,
+        unique=True,
+        help_text="public key"
+    )
+
+    secret_key = models.CharField(
+        verbose_name="secret key",
+        max_length=100,
+        blank=False,
+        null=False,
+        help_text="secret key"
+    )
+
+    account = models.ForeignKey(
+        SocialAccount, on_delete=models.SET_NULL, related_name="inboxes", null=True
+    )
+
+    network = models.ForeignKey(
+        'blockchain.Network', on_delete=models.PROTECT, related_name="inboxes"
+    )
+
+    sns = models.ForeignKey('sns.SNS', on_delete=models.PROTECT, related_name="inboxes")
+
+    def __str__(self):
+        return f"{self.account.nickname}({self.sns.name}) {self.pub_key}"
