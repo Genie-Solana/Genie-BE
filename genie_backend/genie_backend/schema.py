04b0bd544382d9bb6a218b97fa79ecd1436be44b
김상엽 <sangsang@gimsang-yeob-ui-MacBookPro-2.local>
2023-06-26 21:10:22 +0900

    Add: GetSocialAccountInfo query
    Query for get Social Layer Account's pub_key, nickname
    
     #25

--- genie_backend/genie_backend/schema.py
+++ genie_backend/genie_backend/schema.py
@@ -1,5 +1,6 @@
 import graphene
 
+from accounts.graphql.user_query import AccountQuery
 from sns.graphql.server_query import ServerQuery
 from blockchain.graphql.wallet_mutation import WalletMutation
 from sns.graphql.server_mutation import ServerMutation
@@ -7,6 +8,7 @@ from sns.graphql.sns_mutation import SNSMutation
 
 
 class Query(
+    AccountQuery,
     ServerQuery,
     graphene.ObjectType
 ):
