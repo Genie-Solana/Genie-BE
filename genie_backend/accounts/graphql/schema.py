190794cee39b8a31415d96c39a0a9d91740f80b9
Sangyeup <sang2668@snu.ac.kr>
2023-06-28 19:24:44 +0900

    Add: GetUserInfo Query except profile_img
     #30

--- genie_backend/accounts/graphql/schema.py
+++ genie_backend/accounts/graphql/schema.py
@@ -1,9 +1,29 @@
 import graphene
-from accounts.models import SocialAccount
+from accounts.models import SocialAccount, Inbox
+from blockchain.models import Network
 from graphene_django.types import DjangoObjectType
 
 
+class NetworkType(DjangoObjectType):
+    class Meta:
+        model = Network
+        fields = ("name",)
+
+
+class InboxType(DjangoObjectType):
+    class Meta:
+        model = Inbox
+        fields = ("pub_key", "network")
+
+
 class GetAccountInfoReturnType(graphene.ObjectType):
-    success = graphene.NonNull(graphene.Boolean)
     pub_key = graphene.NonNull(graphene.String)
     nickname = graphene.NonNull(graphene.String)
+
+
+class GetUserInfoReturnType(graphene.ObjectType):
+    #sns_profile_img = 
+    sns_nickname = graphene.NonNull(graphene.String)
+    account_pub_key = graphene.NonNull(graphene.String)
+    account_nickname = graphene.NonNull(graphene.String)
+    inbox_list = graphene.List(graphene.NonNull(InboxType))
--- genie_backend/accounts/graphql/user_query.py
+++ genie_backend/accounts/graphql/user_query.py
@@ -1,7 +1,8 @@
 from typing import Optional
 import graphene
 from django.db.models import QuerySet
-from accounts.graphql.schema import GetAccountInfoReturnType
+from accounts.models import Inbox
+from accounts.graphql.schema import InboxType, GetAccountInfoReturnType, GetUserInfoReturnType
 from sns.models import SNS, SNSConnectionInfo
 from genie_backend.utils import errors
 
@@ -12,17 +13,45 @@ class AccountQuery(graphene.ObjectType):
         sns_name=graphene.String(required=True),
         discriminator=graphene.String(required=True),
     )
+    
+    get_user_info = graphene.NonNull(
+        GetUserInfoReturnType,
+        sns_name=graphene.String(required=True),
+        discriminator=graphene.String(required=True),
+    )
 
     def resolve_get_social_account_info(
         self, info: graphene.ResolveInfo, **kwargs
     ):
         sns_name = kwargs.get("sns_name")
-        handle = kwargs.get("discriminator")
+        discriminator = kwargs.get("discriminator")
         
-        try:
-            sns = SNS.get_by_name(sns_name)
-            social_account = SNSConnectionInfo.get_account(sns, discriminator)
-        except:
-            raise errors.AccountNotFound
+        sns = SNS.get_by_name(sns_name)
+        social_account = SNSConnectionInfo.get_account(sns, discriminator)
 
-        return GetAccountInfoReturnType(success=True, pub_key=social_account.pub_key, nickname=social_account.nickname)
+        return GetAccountInfoReturnType(pub_key=social_account.pub_key, nickname=social_account.nickname)
+
+    def resolve_get_user_info(
+        self, info: graphene.ResolveInfo, **kwargs
+    ):
+        sns_name = kwargs.get("sns_name")
+        discriminator = kwargs.get("discriminator")
+
+        sns = SNS.get_by_name(sns_name)
+        social_account = SNSConnectionInfo.get_account(sns, discriminator)
+        sns_connection_info = SNSConnectionInfo.get_sns_connection(sns, social_account, discriminator)
+
+        #sns_profile_img = sns_connection_info.profile_img
+        sns_nickname = sns_connection_info.handle
+        account_pub_key = social_account.pub_key
+        account_nickname = social_account.nickname
+        inbox_list = Inbox.objects.filter(account=social_account)
+
+        return GetUserInfoReturnType(
+            #sns_profile_img=sns_profile_img,
+            sns_nickname=sns_nickname,
+            account_pub_key=account_pub_key,
+            account_nickname=account_nickname,
+            inbox_list=inbox_list,
+        )
+
